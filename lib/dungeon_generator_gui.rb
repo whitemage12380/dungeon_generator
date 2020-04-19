0a701770c510b9a2c38a55f0f612f3b87e338a27
Egan Neuhengen <lightningworks@gmail.com>
2020-04-18 17:05:32 -0700

    Improved UI, Click on map objects to get their name

--- lib/dungeon_generator_gui.rb
+++ lib/dungeon_generator_gui.rb
@@ -31,8 +31,10 @@ class FillRectangleShape < RectangleShape
 end
 
 class DungeonGeneratorGui < FXMainWindow
+  include DungeonGeneratorHelper
 
   SQUARE_PIXELS = 16
+  SIDEBAR_PIXELS = 120
   EDGE_COORDINATES = {
     north: [0, 0, 1, 0],
     east: [1, 0, 1, 1],
@@ -42,7 +44,7 @@ class DungeonGeneratorGui < FXMainWindow
 
   def initialize(app, map)
     canvas_length = (map.size+1) * SQUARE_PIXELS
-    window_width = canvas_length
+    window_width = canvas_length + SIDEBAR_PIXELS
     window_height = canvas_length + 20
     super(app, "Dungeon Generator", :width => window_width, :height => window_height)
     # Menu bar
@@ -50,21 +52,40 @@ class DungeonGeneratorGui < FXMainWindow
     file_menu = FXMenuPane.new(self)
     FXMenuCommand.new(file_menu, "&New...")
     FXMenuTitle.new(menu_bar, "&File", nil, file_menu)
-    #FXButton.new(self, "&Test", nil, app, FXApp::ID_QUIT)
-    #@canvas = FXCanvas.new(self, :opts => LAYOUT_FILL_X|LAYOUT_FILL_Y|LAYOUT_TOP|LAYOUT_LEFT)
-    frame = FXHorizontalFrame.new(self,
-      LAYOUT_FILL_X|LAYOUT_FILL_Y|FRAME_SUNKEN|FRAME_THICK,
-      0, 0, 0, 0, 0, 0, 0, 0)
-    scroll_area = FXScrollWindow.new(frame, :opts => LAYOUT_FILL)
-      #LAYOUT_FIX_X|LAYOUT_FIX_Y,
-      #0, 0, map.size * SQUARE_PIXELS, map.size * SQUARE_PIXELS)
-    @canvas = ShapeCanvas.new(scroll_area, nil, 0, LAYOUT_FILL_X|LAYOUT_FILL_Y)
-    #     @canvas = ShapeCanvas.new(scroll_area, nil, 0, LAYOUT_FIX_X|LAYOUT_FIX_Y, 0, 0, canvas_length, canvas_length)
+    # Main Window Structure
+    @frame = FXHorizontalFrame.new(self,
+      LAYOUT_SIDE_TOP|LAYOUT_FILL_X|LAYOUT_FILL_Y|FRAME_SUNKEN|FRAME_THICK,
+      #FRAME_SUNKEN|FRAME_THICK,
+      0, 0, 800, 800, 0, 0, 0, 0)
+    @left_frame = FXVerticalFrame.new(@frame, 
+      FRAME_SUNKEN|LAYOUT_FILL_X|LAYOUT_FILL_Y|LAYOUT_TOP|LAYOUT_LEFT,
+      :padLeft => 10, :padRight => 10, :padTop => 10, :padBottom => 10)   
+    @right_frame = FXVerticalFrame.new(@frame, 
+      FRAME_SUNKEN|LAYOUT_FILL_Y|LAYOUT_TOP|LAYOUT_LEFT,
+      :padLeft => 10, :padRight => 10, :padTop => 10, :padBottom => 10)
+    # Left Pane
+    @dungeon_name = FXLabel.new(@left_frame, "Dungeon", nil, JUSTIFY_CENTER_X|LAYOUT_FILL_X)
+    FXHorizontalSeparator.new(@left_frame, SEPARATOR_GROOVE|LAYOUT_FILL_X)
+    @canvas = ShapeCanvas.new(@left_frame, nil, 0,
+      LAYOUT_FILL_X|LAYOUT_FILL_Y|LAYOUT_TOP|LAYOUT_LEFT, 0, 0, canvas_length, canvas_length)
+    # Right Pane
+    @info_title = FXLabel.new(@right_frame, "Info", nil, JUSTIFY_CENTER_X|LAYOUT_FILL_X)
+    FXHorizontalSeparator.new(@right_frame, SEPARATOR_RIDGE|LAYOUT_FILL_X)
+
+    draw_canvas(@canvas, map)
+    connect_canvas(@canvas, map)
+  end
+
+  def draw_canvas(canvas, map)
+    canvas.backColor = FXColor::White
+    if $configuration['background_display'] == 'dark'
+      canvas.backColor = FXColor::DimGray
+    end
     shapes = ShapeGroup.new()
-    draw_grid(shapes, $configuration['map_size'], $configuration['map_size'])
     draw_map(shapes, map)
-    @canvas.scene = shapes
-    #shapes.each { |s| puts s.lineStyle if s.is_a? LineShape}
+    draw_grid(shapes, $configuration['map_size'], $configuration['map_size'])
+    draw_empty(shapes, map)
+    canvas.scene = shapes
   end
 
   def draw_map(shapes, map)
@@ -75,19 +96,33 @@ class DungeonGeneratorGui < FXMainWindow
     }
   end
 
+  def draw_grid(shapes, width, height)
+    width.times { |x|
+      height.times { |y|
+        px = x * SQUARE_PIXELS
+        py = y * SQUARE_PIXELS
+        dot = CircleShape.new(px, py, 0.5)
+        dot.foreground = FXColor::BlanchedAlmond
+        shapes.addShape(dot)
+      }
+    }
+  end
+
+  def draw_empty(shapes, map)
+    map.xlength.times { |x|
+      map.ylength.times { |y|
+        draw_empty_square(shapes, x, y) if map.square(x: x, y: y).nil?
+      }
+    }
+  end
+
   def draw_map_square(shapes, x, y, square)
     base_px = x * SQUARE_PIXELS
     base_py = y * SQUARE_PIXELS
-    if square.nil?
-      if $configuration['background_display'] == 'dark'
-        shape = FillRectangleShape.new(
-          base_px, base_py, SQUARE_PIXELS, SQUARE_PIXELS
-        )
-        shape.foreground = FXColor::DimGray
-        shapes.addShape(shape)
-        return
-      end
-    end
+    return if square.nil?
+    shape = FillRectangleShape.new(base_px, base_py, SQUARE_PIXELS, SQUARE_PIXELS)
+    shape.foreground = FXColor::WhiteSmoke
+    shapes.addShape(shape)
     if $configuration['map_display'] == 'debug'
       max_pixels = SQUARE_PIXELS - 1
       min_pixels = 1
@@ -122,16 +157,30 @@ class DungeonGeneratorGui < FXMainWindow
     }
   end
 
-  def draw_grid(shapes, width, height)
-    width.times { |x|
-      height.times { |y|
-        px = x * SQUARE_PIXELS
-        py = y * SQUARE_PIXELS
-        dot = CircleShape.new(px, py, 0.5)
-        dot.foreground = FXColor::BlanchedAlmond
-        shapes.addShape(dot)
-      }
-    }
+  def draw_empty_square(shapes, x, y)
+    base_px = x * SQUARE_PIXELS
+    base_py = y * SQUARE_PIXELS
+    if $configuration['background_display'] == 'dark'
+      shape = FillRectangleShape.new(
+        base_px, base_py, SQUARE_PIXELS, SQUARE_PIXELS
+      )
+      shape.foreground = FXColor::DimGray
+      shapes.addShape(shape)
+    end
+  end
+
+  def connect_canvas(canvas, map)
+    canvas.connect(SEL_LEFTBUTTONPRESS) do |sender, selector, event|
+      selected_map_coordinates = {x: event.click_x / SQUARE_PIXELS, y: event.click_y / SQUARE_PIXELS}
+      selected_square = map.square(selected_map_coordinates)
+      if selected_square.nil?
+        log "Clicked an empty square or outside the map boundary (#{selected_map_coordinates})"
+        next
+      end
+      selected_map_object = selected_square.map_object
+      log "Selected #{selected_map_object.name}"
+      @info_title.text = selected_map_object.name
+    end
   end
 
   def create
@@ -140,10 +189,3 @@ class DungeonGeneratorGui < FXMainWindow
   end
 
 end
-
-#application = FXApp.new("Hello", "FoxTest")
-#main = FXMainWindow.new(application, "Hello", nil, nil, DECOR_ALL)
-#FXButton.new(main, "&Hello, World!", nil, application, FXApp::ID_QUIT)
-#application.create()
-#main.show(PLACEMENT_SCREEN)
-#application.run()
